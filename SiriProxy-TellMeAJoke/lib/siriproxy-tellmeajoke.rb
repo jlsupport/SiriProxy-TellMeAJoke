6e81d21d6942179a9d4d3898e9b8ce48d14b95f8
Lars Jørgensen <lars@jlsupport.dk>
2011-11-29 21:13:42 +0100

    changed listen for

--- SiriProxy-TellMeAJoke/lib/siriproxy-tellmeajoke.rb
+++ SiriProxy-TellMeAJoke/lib/siriproxy-tellmeajoke.rb
@@ -11,64 +11,28 @@ require 'nokogiri'
 ######
 
 class SiriProxy::Plugin::TellMeAJoke < SiriProxy::Plugin
+
+	@apikey = "c11adefe5adbd439671566750f43704af03eb354b9d7e520ac"
+	
   def initialize(config)
     #if you have custom configuration options, process them here!
   end
-
-  listen_for /test siri proxy/i do
-    say "Siri Proxy is up and running!" #say something to the user!
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-
-  #demonstrate state change
-  listen_for /siri proxy test state/i do
-    set_state :some_state #set a state... this is useful when you want to change how you respond after certain conditions are met!
-    say "I set the state, try saying 'confirm state change'"
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-  
-  listen_for /confirm state change/i, within_state: :some_state do #this only gets processed if you're within the :some_state state!
-    say "State change works fine!"
-    set_state nil #clear out the state!
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-  
-  #demonstrate asking a question
-  listen_for /siri proxy test question/i do
-    response = ask "Is this thing working?" #ask the user for something
-    
-    if(response =~ /yes/i) #process their response
-      say "Great!" 
-    else
-      say "You could have just said 'yes'!"
-    end
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-  
-  #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
-  listen_for /siri proxy number ([0-9,]*[0-9])/i do |number|
-    say "Detected number: #{number}"
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-  
-  #demonstrate injection of more complex objects without shortcut methods.
-  listen_for /test map/i do
-    add_views = SiriAddViews.new
-    add_views.make_root(last_ref_id)
-    map_snippet = SiriMapItemSnippet.new
-    map_snippet.items << SiriMapItem.new
-    utterance = SiriAssistantUtteranceView.new("Testing map injection!")
-    add_views.views << utterance
-    add_views.views << map_snippet
-    
-    #you can also do "send_object object, target: :guzzoni" in order to send an object to guzzoni
-    send_object add_views #send_object takes a hash or a SiriObject object
-    
-    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
+	
+	# Listen
+	listen_for(/tell me a joke/i) { tell_random_joke }
+	listen_for(/tell me a (.*) joke/i) { |category| tell_category_joke(category) }
+	
+	def tell_random_joke
+		#say "I don`t know any good jokes yet"
+		say "A young blonde woman is distraught because she fears her husband is having an affair, so she goes to a gun shop and buys a handgun. The next day she comes home to find her husband in bed with a beautiful redhead. She grabs the gun and holds it to her own head. The husband jumps out of bed, begging and pleading with her not to shoot herself. Hysterically the blonde responds to the husband, 'Shut up...youre next!'" 
+		
+		request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+	end
+	
+	def tell_category_joke(temp)
+		say "I don`t know any good jokes yet"
+		
+		request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+	end
+	
 end
