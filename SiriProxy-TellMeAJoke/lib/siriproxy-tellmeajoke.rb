92dd661fb1158d83345bf30fb51c719df8df99e8
Lars Jørgensen <lars@jlsupport.dk>
2011-12-05 17:40:38 +0100

    dd

--- SiriProxy-TellMeAJoke/lib/siriproxy-tellmeajoke.rb
+++ SiriProxy-TellMeAJoke/lib/siriproxy-tellmeajoke.rb
@@ -6,40 +6,41 @@ require 'nokogiri'
 #############
 # This is a plugin for SiriProxy to get siri telling you jokes from jokeit.net
 #
-# Example usage: "Tell me a joke" or "Tell me a blonde joke"
+# Example usage: "Tell me a joke"
 # 
 # Request a apikey at support@jokeit.net
 #
 #############
 
 class SiriProxy::Plugin::TellMeAJoke < SiriProxy::Plugin
-
-	@apikey = "c11adefe5adbd439671566750f43704af03eb354b9d7e520ac"
+	
+	@@apikey = ""
 	
   def initialize(config)
-    #if you have custom configuration options, process them here!
   end
 	
 	# Listen
 	listen_for(/tell me a joke/i) { tell_random_joke }
-	listen_for(/tell me a (.*) joke/i) { |category| tell_category_joke(category) }
 	
 	def tell_random_joke
-		if(@apikey == "")
-			say "Invalid api key"
+		if(@@apikey == "")
+			say "Invalid API key"
 		else
-			doc = Nokogiri::XML(open("http://www.jokeit.net/api/siriproxy/getjoke.php"))
+			doc = Nokogiri::XML(open("http://www.jokeit.net/api/siriproxy/getjoke.php?apikey=#{@@apikey}"))
 			root = doc.xpath('//root').map do |i|
-			status = i.xpath('status')
+			@status = i.xpath('status').first.text()
+			end
 			
-			say "Status is: " + status
-		
-		request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-	end
-	
-	def tell_category_joke(category)
-		say "I don`t know any good jokes yet"
-		
+			if(@status == "OK")
+				root = doc.xpath('//root').map do |i|
+				@joke = i.xpath('joke').first.text()
+				end
+				say "" + @joke
+			else
+				say "" + @status
+			end
+
+		end
 		request_completed #always complete your request! Otherwise the phone will "spin" at the user!
 	end
 	
